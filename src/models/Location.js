886087e2e049eabaca36043fa2d8b4e59e7c2a6d
ChristinaDian <krisdian2001@gmail.com>
2024-01-13 23:28:54 +0200

    Front end

--- src/models/Location.js
+++
@@ -1,9 +0,0 @@
-class Location {
-    constructor(locationId, street, number, city, country) {
-        this.locationId = locationId;
-        this.street = street;
-        this.number = number;
-        this.city = city;
-        this.country = country;
-    }
-}
--- src/models/Reservation.js
+++
@@ -1,8 +0,0 @@
-class Reservation {
-    constructor(id, contactName, phoneNumber, holiday) {
-        this.id          = id;
-        this.contactName = contactName;
-        this.phoneNumber = phoneNumber;
-        this.holiday     = holiday
-    }
-}
